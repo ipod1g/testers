09a03450890fb1287467190bd5ee3747386b03c0
ipod1g <kubonkwan99@gmail.com>
2023-04-09 22:55:09 +0800

    Test render deployment

--- riot-login-clone/client/src/components/LoginSection.jsx
+++ riot-login-clone/client/src/components/LoginSection.jsx
@@ -1,208 +1,206 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
-import LoginModal from "./LoginModal";
-import Axios from "axios";
-import "./LoginSection.css";
-import SocialLoginButton from "./SocialLoginButton";
-import LoginForm from "./LoginForm";
-import Loading from "./Loading";
-import Version from "./Version";
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import LoginModal from './LoginModal';
+import Axios from 'axios';
+import './LoginSection.css';
+import SocialLoginButton from './SocialLoginButton';
+import LoginForm from './LoginForm';
+import Loading from './Loading';
+import Version from './Version';
 
 function LoginSection() {
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
-   const [passwordShown, setPasswordShown] = useState(false);
-   const [isError, setIsError] = useState(false);
-   const [errorMsg, setErrorMsg] = useState("");
-   const [isPwFocused, setIsPwFocused] = useState(false);
-   const idInputRef = useRef(null);
-   const passwordInputRef = useRef(null);
-   const [isFirstRun, setIsFirstRun] = useState(true);
-   const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isPwFocused, setIsPwFocused] = useState(false);
+  const idInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+  const [isFirstRun, setIsFirstRun] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-   const [loginStatus, setLoginStatus] = useState("");
-   const [loginModalShown, setLoginModalShown] = useState(false);
+  const [loginStatus, setLoginStatus] = useState('');
+  const [loginModalShown, setLoginModalShown] = useState(false);
 
-   const login = () => {
-      Axios.post("https://riot-clone-login-api.herokuapp.com/login", {
-         username: username,
-         password: password,
-      }).then(
-         (response) => {
-            setIsLoading(false);
-            console.log(response);
-            setIsError(false);
-            setLoginModalShown(true);
-         },
-         (error) => {
-            setIsLoading(false);
-            console.log(error);
-            setErrorMsg(
-               "Your login credentials don't match an account in our system."
-            );
-            setIsError(true);
-         }
-      );
-   };
-
-   const submitHandler = (e) => {
-      login();
-      setIsLoading(true);
-      e.preventDefault();
-   };
-
-   useEffect(() => {
-      if (isFirstRun) {
-         setIsFirstRun(false);
-      } else {
-         passwordInputRef.current.focus();
+  const login = () => {
+    Axios.post('https://riotlogin-clone-server.onrender.com/login', {
+      username: username,
+      password: password,
+    }).then(
+      (response) => {
+        setIsLoading(false);
+        console.log(response);
+        setIsError(false);
+        setLoginModalShown(true);
+      },
+      (error) => {
+        setIsLoading(false);
+        console.log(error);
+        setErrorMsg(
+          "Your login credentials don't match an account in our system."
+        );
+        setIsError(true);
       }
-   }, [passwordShown]);
+    );
+  };
 
-   useEffect(() => {
-      if (isFirstRun) {
-         setIsFirstRun(false);
-      } else if (isError === true) {
-         idInputRef.current.focus();
-         setPassword("");
-      }
-   }, [isError]);
+  const submitHandler = (e) => {
+    login();
+    setIsLoading(true);
+    e.preventDefault();
+  };
 
-   const loginRoutes = ["fb", "google", "apple"];
+  useEffect(() => {
+    if (isFirstRun) {
+      setIsFirstRun(false);
+    } else {
+      passwordInputRef.current.focus();
+    }
+  }, [passwordShown]);
 
-   return (
-      <>
-         {isLoading ? (
-            <Loading formType={"login"} />
-         ) : (
-            <section className="form-main">
-               <div className="form-outer">
-                  <img
-                     className="logo-main"
-                     src={require("../assets/002_RG_2021_FULL_LOCKUP_RED.png")}
-                     alt="Riot games logo"
-                  />
-                  <h2 className="sign-in">Sign in</h2>
-                  {isError !== false ? (
-                     <div className="error-shadow">
-                        <span className="error-box">
-                           <span className="error-msg">{errorMsg}</span>
-                        </span>
-                     </div>
-                  ) : (
-                     ""
-                  )}
-                  <div className="form-innerwrap">
-                     <form
-                        id="main-form"
-                        onSubmit={submitHandler}
-                        className="form-inner"
-                        autoComplete="off"
-                     >
-                        <LoginForm
-                           formType="username"
-                           details={username}
-                           isError={isError}
-                           setIsError={setIsError}
-                           setDetails={setUsername}
-                           refInput={idInputRef}
-                        />
+  useEffect(() => {
+    if (isFirstRun) {
+      setIsFirstRun(false);
+    } else if (isError === true) {
+      idInputRef.current.focus();
+      setPassword('');
+    }
+  }, [isError]);
 
-                        <LoginForm
-                           formType="password"
-                           details={password}
-                           isError={isError}
-                           setIsError={setIsError}
-                           setDetails={setPassword}
-                           refInput={passwordInputRef}
-                           setIsPwFocused={setIsPwFocused}
-                           pwShown={passwordShown}
-                        />
-                        <div className="toggle-wrapper">
-                           <button
-                              type="button"
-                              className={
-                                 isPwFocused
-                                    ? passwordShown
-                                       ? "toggle-btn toggle-btn-show "
-                                       : "toggle-btn toggle-btn-hide"
-                                    : "invis-btn"
-                              }
-                              onClick={() => {
-                                 setPasswordShown(!passwordShown);
-                              }}
-                              onFocus={() => {
-                                 setIsPwFocused(true);
-                              }}
-                              onBlur={() => {
-                                 setIsPwFocused(false);
-                              }}
-                              tabIndex="-1"
-                           ></button>
-                        </div>
-                     </form>
-                  </div>
-                  <ul className="login-routes">
-                     {loginRoutes.map((social) => (
-                        <SocialLoginButton
-                           key={social}
-                           social={social}
-                           setLoginModalShown={setLoginModalShown}
-                        />
-                     ))}
-                  </ul>
-                  <CSSTransition
-                     in={loginModalShown}
-                     timeout={800}
-                     unmountOnExit
-                     classNames="modal-anim"
-                  >
-                     <LoginModal
-                        open={loginModalShown}
-                        onClose={() => setLoginModalShown(false)}
-                        setModalOpen={setLoginModalShown}
-                        variant="primary"
-                     ></LoginModal>
-                  </CSSTransition>
+  const loginRoutes = ['fb', 'google', 'apple'];
 
-                  <label className="stay-container" tabIndex={0}>
-                     <input type="checkbox" value="stay" />
-                     Stay signed in
-                     <span className="checkmark"></span>
-                  </label>
-               </div>
+  return (
+    <>
+      {isLoading ? (
+        <Loading formType={'login'} />
+      ) : (
+        <section className="form-main">
+          <div className="form-outer">
+            <img
+              className="logo-main"
+              src={require('../assets/002_RG_2021_FULL_LOCKUP_RED.png')}
+              alt="Riot games logo"
+            />
+            <h2 className="sign-in">Sign in</h2>
+            {isError !== false ? (
+              <div className="error-shadow">
+                <span className="error-box">
+                  <span className="error-msg">{errorMsg}</span>
+                </span>
+              </div>
+            ) : (
+              ''
+            )}
+            <div className="form-innerwrap">
+              <form
+                id="main-form"
+                onSubmit={submitHandler}
+                className="form-inner"
+                autoComplete="off"
+              >
+                <LoginForm
+                  formType="username"
+                  details={username}
+                  isError={isError}
+                  setIsError={setIsError}
+                  setDetails={setUsername}
+                  refInput={idInputRef}
+                />
 
-               <footer className="bottom-section">
-                  <div className="login-btn-container case1">
-                     <button
-                        form="main-form"
-                        type="submit"
-                        className={
-                           username && password
-                              ? "login-btn--on"
-                              : "login-btn--off"
-                        }
-                     ></button>
-                  </div>
+                <LoginForm
+                  formType="password"
+                  details={password}
+                  isError={isError}
+                  setIsError={setIsError}
+                  setDetails={setPassword}
+                  refInput={passwordInputRef}
+                  setIsPwFocused={setIsPwFocused}
+                  pwShown={passwordShown}
+                />
+                <div className="toggle-wrapper">
+                  <button
+                    type="button"
+                    className={
+                      isPwFocused
+                        ? passwordShown
+                          ? 'toggle-btn toggle-btn-show '
+                          : 'toggle-btn toggle-btn-hide'
+                        : 'invis-btn'
+                    }
+                    onClick={() => {
+                      setPasswordShown(!passwordShown);
+                    }}
+                    onFocus={() => {
+                      setIsPwFocused(true);
+                    }}
+                    onBlur={() => {
+                      setIsPwFocused(false);
+                    }}
+                    tabIndex="-1"
+                  ></button>
+                </div>
+              </form>
+            </div>
+            <ul className="login-routes">
+              {loginRoutes.map((social) => (
+                <SocialLoginButton
+                  key={social}
+                  social={social}
+                  setLoginModalShown={setLoginModalShown}
+                />
+              ))}
+            </ul>
+            <CSSTransition
+              in={loginModalShown}
+              timeout={800}
+              unmountOnExit
+              classNames="modal-anim"
+            >
+              <LoginModal
+                open={loginModalShown}
+                onClose={() => setLoginModalShown(false)}
+                setModalOpen={setLoginModalShown}
+                variant="primary"
+              ></LoginModal>
+            </CSSTransition>
 
-                  <nav>
-                     <ul>
-                        <li>
-                           <Link to="/register">can't sign in?</Link>
-                        </li>
-                     </ul>
-                     <ul>
-                        <li>
-                           <Link to="/register">create account</Link>
-                        </li>
-                     </ul>
-                     <Version />
-                  </nav>
-               </footer>
-            </section>
-         )}
-      </>
-   );
+            <label className="stay-container" tabIndex={0}>
+              <input type="checkbox" value="stay" />
+              Stay signed in
+              <span className="checkmark"></span>
+            </label>
+          </div>
+
+          <footer className="bottom-section">
+            <div className="login-btn-container case1">
+              <button
+                form="main-form"
+                type="submit"
+                className={
+                  username && password ? 'login-btn--on' : 'login-btn--off'
+                }
+              ></button>
+            </div>
+
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/register">can't sign in?</Link>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <Link to="/register">create account</Link>
+                </li>
+              </ul>
+              <Version />
+            </nav>
+          </footer>
+        </section>
+      )}
+    </>
+  );
 }
 export default LoginSection;
--- riot-login-clone/client/src/components/Register.jsx
+++ riot-login-clone/client/src/components/Register.jsx
@@ -1,159 +1,159 @@
-import React, { useState, useRef, useEffect } from "react";
-import Axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import RegForm from "./RegForm";
-import "./Register.css";
-import Divider from "../assets/reg/divider.png";
-import PEGI from "../assets/reg/pegi.jpg";
-import Riotlogofull from "../assets/Riot Games RGB Logos (Web)/002_RG 2021 Full Lockup/002_RG_2021_FULL_LOCKUP_OFFWHITE.png";
-import Loading from "./Loading";
+import React, { useState, useRef, useEffect } from 'react';
+import Axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import RegForm from './RegForm';
+import './Register.css';
+import Divider from '../assets/reg/divider.png';
+import PEGI from '../assets/reg/pegi.jpg';
+import Riotlogofull from '../assets/Riot Games RGB Logos (Web)/002_RG 2021 Full Lockup/002_RG_2021_FULL_LOCKUP_OFFWHITE.png';
+import Loading from './Loading';
 
 const Register = () => {
-   const [usernameReg, setUsernameReg] = useState("");
-   const [passwordReg, setPasswordReg] = useState("");
-   const [passwordShown, setPasswordShown] = useState(false);
-   const [isPwFocused, setIsPwFocused] = useState(false);
-   const passwordInputRef = useRef(null);
-   const [isFirstRun, setIsFirstRun] = useState(true);
-   const [isLoading, setIsLoading] = useState(false);
-   const navigate = useNavigate();
+  const [usernameReg, setUsernameReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [isPwFocused, setIsPwFocused] = useState(false);
+  const passwordInputRef = useRef(null);
+  const [isFirstRun, setIsFirstRun] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-   useEffect(() => {
-      if (isFirstRun) {
-         setIsFirstRun(false);
-      } else {
-         passwordInputRef.current.focus();
+  useEffect(() => {
+    if (isFirstRun) {
+      setIsFirstRun(false);
+    } else {
+      passwordInputRef.current.focus();
+    }
+  }, [passwordShown]);
+
+  const register = () => {
+    Axios.post('https://riotlogin-clone-server.onrender.com/register', {
+      username: usernameReg,
+      password: passwordReg,
+    }).then(
+      (response) => {
+        setIsLoading(false);
+        alert('Register SUCCESS!');
+        console.log(response.data);
+        navigate(-1);
+      },
+      (error) => {
+        setIsLoading(false);
+        alert(JSON.stringify(error));
+        console.log(error);
       }
-   }, [passwordShown]);
+    );
+  };
 
-   const register = () => {
-      Axios.post("https://riot-clone-login-api.herokuapp.com/register", {
-         username: usernameReg,
-         password: passwordReg,
-      }).then(
-         (response) => {
-            setIsLoading(false);
-            alert("Register SUCCESS!");
-            console.log(response.data);
-            navigate(-1);
-         },
-         (error) => {
-            setIsLoading(false);
-            alert(JSON.stringify(error));
-            console.log(error);
-         }
-      );
-   };
-
-   return (
-      <article className="regform-container">
-         <header>
-            <a
-               href="https://www.leagueoflegends.com/en-gb/?_gl=1*1ssl8ty*_ga*MTExNzQ5MzYzNy4xNjYxNjg2NzQ2*_ga_FXBJE5DEDD*MTY2MzMwNTg4NS45LjEuMTY2MzMwNjg3Ny42MC4wLjA.&amp;_ga=2.252011597.574815907.1663179802-1117493637.1661686746"
-               target="_blank"
+  return (
+    <article className="regform-container">
+      <header>
+        <a
+          href="https://www.leagueoflegends.com/en-gb/?_gl=1*1ssl8ty*_ga*MTExNzQ5MzYzNy4xNjYxNjg2NzQ2*_ga_FXBJE5DEDD*MTY2MzMwNTg4NS45LjEuMTY2MzMwNjg3Ny42MC4wLjA.&amp;_ga=2.252011597.574815907.1663179802-1117493637.1661686746"
+          target="_blank"
+        >
+          <img
+            className="lol-logo-img"
+            src="https://signup.br.leagueoflegends.com/en-gb/assets/lol-logo.png"
+            alt="lol logo"
+            width="120px"
+          />
+        </a>
+        <nav className="nav-back">
+          <ul>
+            <li>
+              <icon className="left-arrow"></icon>
+              <Link to="/">GO BACK</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section className="scene-content">
+        <header className="header-main">Register Now</header>
+        <h3>Enter your info below</h3>
+        <div className="divider">
+          <img src={Divider} alt="divider gold" />
+        </div>
+        {isLoading ? (
+          <Loading formType="register" />
+        ) : (
+          <div className="regform-inner--wrapper">
+            <form
+              id="regform"
+              onSubmit={register}
+              className="regform-inner"
+              autoComplete="off"
             >
-               <img
-                  className="lol-logo-img"
-                  src="https://signup.br.leagueoflegends.com/en-gb/assets/lol-logo.png"
-                  alt="lol logo"
-                  width="120px"
-               />
-            </a>
-            <nav className="nav-back">
-               <ul>
-                  <li>
-                     <icon className="left-arrow"></icon>
-                     <Link to="/">GO BACK</Link>
-                  </li>
-               </ul>
-            </nav>
-         </header>
-         <section className="scene-content">
-            <header className="header-main">Register Now</header>
-            <h3>Enter your info below</h3>
-            <div className="divider">
-               <img src={Divider} alt="divider gold" />
-            </div>
-            {isLoading ? (
-               <Loading formType="register" />
-            ) : (
-               <div className="regform-inner--wrapper">
-                  <form
-                     id="regform"
-                     onSubmit={register}
-                     className="regform-inner"
-                     autoComplete="off"
-                  >
-                     <RegForm
-                        formType="usernameReg"
-                        details={usernameReg}
-                        setDetails={setUsernameReg}
-                     />
-                     <RegForm
-                        formType="passwordReg"
-                        details={passwordReg}
-                        setDetails={setPasswordReg}
-                        refInput={passwordInputRef}
-                        pwShown={passwordShown}
-                        setIsPwFocused={setIsPwFocused}
-                     />
-                     <div className="reg-toggle-wrapper">
-                        <button
-                           type="button"
-                           className={
-                              //    "toggle-btn toggle-btn-hide colorsvg-gold"
-                              isPwFocused
-                                 ? passwordShown
-                                    ? "toggle-btn toggle-btn-show colorsvg-gold "
-                                    : "toggle-btn toggle-btn-hide colorsvg-gold"
-                                 : "invis-btn"
-                           }
-                           onClick={() => {
-                              setPasswordShown(!passwordShown);
-                           }}
-                           onFocus={() => {
-                              setIsPwFocused(true);
-                           }}
-                           onBlur={() => {
-                              setIsPwFocused(false);
-                           }}
-                           tabIndex="-1"
-                        ></button>
-                     </div>
-                     <div className="reg-btn">
-                        <button
-                           onClick={(e) => {
-                              setIsLoading(true);
-                              register();
-                              e.preventDefault();
-                           }}
-                           tabIndex="0"
-                        >
-                           Register
-                        </button>
-                     </div>
-                  </form>
-               </div>
-            )}
-         </section>
-         <footer>
-            <a href="https://www.riotgames.com" target="_blank">
-               <img
-                  src={Riotlogofull}
-                  width="100px"
-                  style={{ verticalAlign: "middle" }}
-                  alt="Riotlogofull"
-               />
-            </a>
-            <img
-               src={PEGI}
-               height="50px"
-               style={{ verticalAlign: "middle" }}
-               alt="PEGI img"
-            />
-         </footer>
-      </article>
-   );
+              <RegForm
+                formType="usernameReg"
+                details={usernameReg}
+                setDetails={setUsernameReg}
+              />
+              <RegForm
+                formType="passwordReg"
+                details={passwordReg}
+                setDetails={setPasswordReg}
+                refInput={passwordInputRef}
+                pwShown={passwordShown}
+                setIsPwFocused={setIsPwFocused}
+              />
+              <div className="reg-toggle-wrapper">
+                <button
+                  type="button"
+                  className={
+                    //    "toggle-btn toggle-btn-hide colorsvg-gold"
+                    isPwFocused
+                      ? passwordShown
+                        ? 'toggle-btn toggle-btn-show colorsvg-gold '
+                        : 'toggle-btn toggle-btn-hide colorsvg-gold'
+                      : 'invis-btn'
+                  }
+                  onClick={() => {
+                    setPasswordShown(!passwordShown);
+                  }}
+                  onFocus={() => {
+                    setIsPwFocused(true);
+                  }}
+                  onBlur={() => {
+                    setIsPwFocused(false);
+                  }}
+                  tabIndex="-1"
+                ></button>
+              </div>
+              <div className="reg-btn">
+                <button
+                  onClick={(e) => {
+                    setIsLoading(true);
+                    register();
+                    e.preventDefault();
+                  }}
+                  tabIndex="0"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </section>
+      <footer>
+        <a href="https://www.riotgames.com" target="_blank">
+          <img
+            src={Riotlogofull}
+            width="100px"
+            style={{ verticalAlign: 'middle' }}
+            alt="Riotlogofull"
+          />
+        </a>
+        <img
+          src={PEGI}
+          height="50px"
+          style={{ verticalAlign: 'middle' }}
+          alt="PEGI img"
+        />
+      </footer>
+    </article>
+  );
 };
 
 export default Register;
