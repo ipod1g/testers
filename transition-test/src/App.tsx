4d50ee8163b7d5b570e081471a880c6d9090496b
ipod1g <kubonkwan99@gmail.com>
2023-04-28 15:35:24 +0800

    fix: remove barba - incompatible with react

--- transition-test/src/App.tsx
+++ transition-test/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import barba from '@barba/core';
 import { AnimatePresence, animate, motion } from 'framer-motion';
 
 function App() {
@@ -10,50 +9,7 @@ function App() {
     visible: { opacity: 1 },
   };
 
-  barba.init({
-    schema: {
-      prefix: 'data-barba',
-      wrapper: 'wrapper',
-      container: 'container',
-    },
-    transitions: [
-      {
-        name: 'fade-transition',
-        leave(data) {
-          return animate(fadeInOut.visible, fadeInOut.hidden);
-        },
-        enter(data) {
-          return animate(fadeInOut.hidden, fadeInOut.visible);
-        },
-      },
-    ],
-  });
-
-  return (
-    <div className="h-screen w-screen bg-blue-300" data-barba="wrapper">
-      <AnimatePresence>
-        <motion.div
-          animate={transition ? 'visible' : 'hidden'}
-          variants={fadeInOut}
-          initial="hidden"
-          exitBeforeEnter
-          data-barba="container"
-          data-barba-namespace="home"
-        >
-          <div>HOME PAGE</div>
-        </motion.div>
-      </AnimatePresence>
-      <motion.div
-        animate={animate}
-        initial={{ opacity: 0 }}
-        exit={{ opacity: 0 }}
-        data-barba="container"
-        data-barba-namespace="about"
-      >
-        <div>ABOUT PAGE</div>
-      </motion.div>
-    </div>
-  );
+  return <div className="h-screen w-screen bg-blue-300"></div>;
 }
 
 export default App;
