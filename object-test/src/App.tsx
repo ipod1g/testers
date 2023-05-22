4a678bf4460786ae6d9efbab514b5a885f7796ad
Bono <kubonkwan99@gmail.com>
2023-05-22 23:56:44 +0800

    test: timestamp for function elapse

--- object-test/src/App.tsx
+++ object-test/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -20,52 +20,53 @@ function App() {
   };
 
   // creates an object where every filled value is mapped to its own key
-
   /** destructure in this case is way slower
    *  this creates TEST_COUNT objects and throws away the rest except for the final object that is returned
    */
   const destructureReduce = (count = TEST_COUNT) => {
+    const startTime = Date.now();
+
     const values = new Array(count).fill(0).map((_, i) => i);
-    return values.reduce(
+    const calculation = values.reduce(
       (a, v) => ({
         ...a,
         [v]: v,
       }),
       {}
     );
+    const endTime = Date.now();
+    const elapsedTime = endTime - startTime;
+    return elapsedTime;
   };
 
   /** this uses one object that we create and just add on the values as we go
    *  this creates only 1 new object from this whole sequence
    */
   const mutableReduce = (count = TEST_COUNT) => {
+    const startTime = Date.now();
     const values = new Array(count).fill(0).map((_, i) => i);
-    return values.reduce((a: { [key: number]: number }, v) => {
+    const calculation = values.reduce((a: { [key: number]: number }, v) => {
       a[v] = v;
       return a;
     }, {});
+    const endTime = Date.now();
+    const elapsedTime = endTime - startTime;
+    return elapsedTime;
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>Object Test</h1>
+      <p>Count: {TEST_COUNT}</p>
+      <p>
+        <div>Destructure reduce</div>
+        <button onClick={() => console.log(destructureReduce())} />
       </p>
-    </>
+      <p>
+        <div>Mutable reduce</div>
+        <button onClick={() => console.log(mutableReduce())} />
+      </p>
+    </div>
   );
 }
 
