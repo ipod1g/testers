e05bdb3f616667e5171205404f4c29c5bec8040a
Bono <kubonkwan99@gmail.com>
2024-01-08 06:44:06 +0800

    feat: add basic bookmark data component

---
+++ spreadsheet-test/src/Spreadsheet/DataComponents/BookmarkDataComponents.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import type {
+  DataEditorComponent,
+  DataViewerComponent,
+} from 'react-spreadsheet';
+import type { Cell } from '../types';
+import { BookmarkIcon, BookmarkFilledIcon } from '@radix-ui/react-icons';
+
+export const BookmarkDataView: DataViewerComponent<Cell> = ({ cell }) => {
+  return cell?.value === 'bookmarked' ? (
+    <BookmarkFilledIcon width={20} height={20} className="mx-auto" />
+  ) : (
+    <BookmarkIcon width={20} height={20} className="mx-auto" />
+  );
+};
+
+export const BookmarkDataEdit: DataEditorComponent<Cell> = ({
+  cell,
+  onChange,
+}) => {
+  const handleChange = React.useCallback(() => {
+    if (cell?.value === 'bookmarked') {
+      // can use a more definitive string too
+      onChange({ ...cell, value: '' });
+    } else {
+      onChange({ ...cell, value: 'bookmarked' });
+    }
+  }, [cell, onChange]);
+
+  return (
+    <button onClick={handleChange}>
+      {cell?.value === 'bookmarked' ? (
+        <BookmarkFilledIcon width={20} height={20} className="mx-auto" />
+      ) : (
+        <BookmarkIcon width={20} height={20} className="mx-auto" />
+      )}
+    </button>
+  );
+};
