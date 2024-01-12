cb239b77e9714170350f388a1ff5880d85fb239f
Bono <kubonkwan99@gmail.com>
2024-01-12 18:00:28 +0800

    feat: empty data cell behave normally

--- spreadsheet-test/src/Spreadsheet/DataComponents/EmptyDataComponents.tsx
+++ spreadsheet-test/src/Spreadsheet/DataComponents/EmptyDataComponents.tsx
@@ -1,13 +1,32 @@
+import React from 'react';
 import type { Cell } from '../types';
 import type {
   DataEditorComponent,
   DataViewerComponent,
 } from 'react-spreadsheet';
 
-export const EmptyDataView: DataViewerComponent<Cell> = () => {
-  return <span className="w-full"></span>;
+export const EmptyDataView: DataViewerComponent<Cell> = ({ cell }) => {
+  return <span className="w-full">{cell?.value}</span>;
 };
 
-export const EmptyDataEdit: DataEditorComponent<Cell> = () => {
-  return <span className="w-full"></span>;
+export const EmptyDataEdit: DataEditorComponent<Cell> = ({
+  cell,
+  onChange,
+}) => {
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      onChange({ ...cell, value: event.target.value });
+    },
+    [cell, onChange]
+  );
+
+  return (
+    <input
+      autoFocus
+      type="text"
+      className="outline-none w-full h-full"
+      onChange={handleChange}
+      value={cell?.value}
+    />
+  );
 };
