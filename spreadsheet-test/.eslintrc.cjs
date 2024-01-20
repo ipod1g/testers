6ca747b8089387daa22a879040dda4b7fae5db8d
Bono <kubonkwan99@gmail.com>
2024-01-20 16:30:05 +0800

    build: enhance linting & ts config

--- spreadsheet-test/.eslintrc.cjs
+++ spreadsheet-test/.eslintrc.cjs
@@ -1,18 +1,78 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    require.resolve("@vercel/style-guide/eslint/node"),
+    require.resolve("@vercel/style-guide/eslint/typescript"),
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    ".eslintrc.cjs",
+    "vite.config.ts",
+    "vite-env.d.ts",
+    "src/lib",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-console": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "type",
+          "unknown",
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        distinctGroup: false,
+        "newlines-between": "always",
+      },
+    ],
+    "import/no-named-as-default": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
   },
-}
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+};
