9bbcca58d83382d50eb3daf621b866247625dd5f
Bono <kubonkwan99@gmail.com>
2024-01-20 15:56:56 +0800

    add my preferred eslint & ts settings

---
+++ eslint-test/.eslintrc.cjs
@@ -0,0 +1,77 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    require.resolve("@vercel/style-guide/eslint/node"),
+    require.resolve("@vercel/style-guide/eslint/typescript"),
+  ],
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    ".eslintrc.cjs",
+    "vite.config.ts",
+    "vite-env.d.ts",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-console": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "type",
+          "unknown",
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        distinctGroup: false,
+        "newlines-between": "always",
+      },
+    ],
+    "import/no-named-as-default": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+};
